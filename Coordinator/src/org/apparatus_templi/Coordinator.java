5071b091fccc82eb65e42453aedc53a777253e4e
Jonathan Nelson <ciasaboark@gmail.com>
2014-01-22 14:29:32 -0500

    Added stub methods -Coordinator: added sub methods for sendMessage() and main() 5 min

--- Coordinator/src/org/apparatus_templi/Coordinator.java
+++ Coordinator/src/org/apparatus_templi/Coordinator.java
@@ -1,5 +1,11 @@
 package org.apparatus_templi;
 
 public class Coordinator {
-
+	public void sendMessage(String name, String message) {
+		
+	}
+	
+	public static void main(String argv[]) {
+		
+	}
 }
