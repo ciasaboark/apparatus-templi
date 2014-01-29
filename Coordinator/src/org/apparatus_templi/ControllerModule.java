357845bfbbff0524eefba605f2bc6b45e221df3c
Jonathan Nelson <ciasaboark@gmail.com>
2014-01-28 21:48:57 -0500

    Continuing eclipse project move

--- Coordinator/src/org/apparatus_templi/ControllerModule.java
+++
@@ -1,7 +0,0 @@
-package org.apparatus_templi;
-
-public interface ControllerModule extends Driver {
-	public String getControllerListXML();
-	public String getControllerStatusXML(String controllerName);
-	public void tellController(String controllerName, String command);
-}
