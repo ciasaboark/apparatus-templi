6391003c589e1eaaf42a330fdea63a637c37d4bf
haglerchristopher <haglerchristopher@gmail.com>
2014-02-10 00:02:51 -0500

    Added an HttpRequestHandler class to handle the http request from the webserver.

---
+++ coordinator_eclipse_workspace/Coordinator/src/org/apparatus_templi/HttpRequestHandler.java
@@ -0,0 +1,15 @@
+package org.apparatus_templi;
+
+import java.io.IOException;
+
+import com.sun.net.httpserver.*;
+
+public class HttpRequestHandler implements HttpHandler  {
+
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
