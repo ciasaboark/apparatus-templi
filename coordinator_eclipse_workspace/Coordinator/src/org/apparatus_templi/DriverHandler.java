2e162b0d14721805ec7f68133b6d3a5234ecfc7f
haglerchristopher <haglerchristopher@gmail.com>
2014-02-20 01:33:48 -0500

    More work done on the XML Response

--- coordinator_eclipse_workspace/Coordinator/src/org/apparatus_templi/DriverHandler.java
+++ coordinator_eclipse_workspace/Coordinator/src/org/apparatus_templi/DriverHandler.java
@@ -9,10 +9,20 @@ import com.sun.net.httpserver.HttpHandler;
 public class DriverHandler implements HttpHandler {
     
 	public void handle(HttpExchange exchange) throws IOException {
-        byte[] response = "<?xml version=\"1.0\"?>\n<resource id=\"1234\" name=\"test\" />\n".getBytes();
-        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 
-            response.length);
+		byte[] response = getResponse();
+        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, response.length);
         exchange.getResponseBody().write(response);
         exchange.close();
     };
+    
+    public byte[] getResponse() {
+    	/*
+    	 * LedFlash is just a place holder for now. It will need to be replaced by the driver name
+    	 * from the GET Request
+    	 */
+    	
+    	String xml = Coordinator.requestWidgetXML("LedFlash");
+    	byte[] response = ("<?xml version=\"1.0\"?>\n" + xml).getBytes();
+    	return response;
+    }
 }
