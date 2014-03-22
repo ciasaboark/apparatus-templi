ef35cf64b0b3489470e00cf1be548f1a9ee48004
Christopher Hagler <haglerchristopher@gmail.com>
2014-03-21 21:37:29 -0400

    more work done to the arduino code for broadcasting messages

--- skeleton/arduino-sketches/echo/library.h
+++ skeleton/arduino-sketches/echo/library.h
@@ -4,22 +4,28 @@
 #include <XBee.h>
 #include <SoftwareSerial.h>
 
-typedef strcut {
-	uint8_t start;
-	uint8_t option;
-	uint8_t data_length;
-	uint8_t fragment_number;
-	uint8_t *data; //pointer to the data buffer
+typedef struct {
+	uint8_t  signature;
+	uint8_t  option;
+	uint8_t  data_length;
+	uint16_t fragment_number;
+	uint8_t  *data; //pointer to the data buffer
 } message_t;
 
+typedef struct {
+	uint8_t   signature;
+	uint8_t   fragment_length;
+	message_t *message;
+} payload;
+
+/* This will populate a message struct from the data supplied by the buffer. 
+   It dynamically allocates memory for the data, then copies it from the buffer
+   to the message struct buffer. It is the responsability of the caller to free the
+   allocated memory */
 void processMessage(message_t *message, uint8_t *buffer);
 
-void processFragment(uint8_t optionsByte, uint8_t dataLength, uint16_t fragmentNumber, String destination, uint8_t data[]);
+void sendCommand(message_t *message);
 
-void sendCommand(String command);
-
-void sendCommandFragment(String commandFragment, int fragmentNo);
-
-void sendBinary(byte data[], int dataLength);
+void sendBinary(byte *buffer, int dataLength);
 
 #endif
