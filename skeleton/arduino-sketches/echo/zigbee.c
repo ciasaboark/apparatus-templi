3c982942c9828a768b622db15ef607c92779cbc7
Christopher Hagler <haglerchristopher@gmail.com>
2014-04-13 18:04:06 -0400

    more work on xbee fragmentation

--- skeleton/arduino-sketches/echo/zigbee.c
+++ skeleton/arduino-sketches/echo/zigbee.c
@@ -1,16 +1,53 @@
 #include "zigbee.h"
 
 Zigbee::Zigbee() {
-	xbee = XBee();
-	softSerial = SoftwareSerial(10, 11);
-	msr = ModemStatusResponse();
-	response = XBeeResponse();
-	rx = ZBRxResponse();
+    xbee = XBee();
+    softSerial = SoftwareSerial(10, 11);
+    msr = ModemStatusResponse();
+    response = XBeeResponse();
+    rx = ZBRxResponse();
 }
 
 Zigbee::~Zigbee() {
 }
 
-uint8_t sendCommand(Message message) {
-	uint8_t success = 0;
+uint8_t Zigbee::start(int serial_port) {
+    uint8_t status = 0;
+    softSerial.begin(serial_port);
+    xbee.begin(soft_serial);
+
+    return status;
+}
+
+uint8_t Zigbee::sendCommand(String message) {
+    uint8_t success = 0;
+    XBeeAddress64 addr64 = XBeeAddress64(0x00000000, 0x00000000);  //coordinator address
+    if(command.length() <= MAX_DATA_SIZE) {
+        Message message((uint8_t)0x0D, uint8_t option, (uint8_t)message.length(), 0, String destination_address, message.toCharArray());
+        ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(message)); //dont think this sizeof is going to work correctly
+        xbee.send(zbTx);
+    }
+    else {
+        int total_fragments = message.length() / MAX_DATA_SIZE;
+        Message message[total_fragments];
+        
+        int begin = 0;
+        int end = MAX_DATA_SIZE;
+        for(int i = 0; i <= message.length() - 1; i = i + 69) {
+            for(int j = 1; j <= total_fragemnts; j++) {
+                
+                /* this is an issue if the command is of odd size because of casting. 
+                 * If message.length is 104 bytes then int total_fragments = message.length() / MAX_DATA_SIZE 
+                 * will be 1. So, it will disccard the rest of the message */				   
+                String message_fragment = message.substring(i, MAX_DATA_SIZE * j); 
+                message[j-1] = Message message((uint8_t)0x0D, uint8_t option, MAX_DATA_SIZE, j, String destination_address, message_fragment.toCharArray()r);
+            }
+        }
+	ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(message)); //dont think this sizeof is going to work correctly
+	xbee.send(zbTx);
+    }	
+}
+
+uint8_t Zigbee::sendBinary(uint8_t array, int length) {
+    uint8_t success = 0;
 }
--- skeleton/arduino-sketches/echo/zigbee.h
+++ skeleton/arduino-sketches/echo/zigbee.h
@@ -7,20 +7,23 @@
 
 #define COORDINATOR_ADDRESS 0x00000000
 #define XBEE_PIN 12
+#define MAX_DATA_SIZE = 32
 
 class Zigbee {
-	private:
-		Xbee xbee;
-		SoftwareSerial softSerial;
-		XBeeResponse response;
-		ZBRxResponse rx;
-		ModemStatusResponse msr;
+    private:
+        Xbee xbee;
+        SoftwareSerial softSerial;
+        XBeeResponse response;
+        ZBRxResponse rx;
+        ModemStatusResponse msr;
 
-	public:
-		Zigbee()
-		~Zigbee();
-
-		uint8_t sendCommand(Message message);
+    public:
+        Zigbee()
+	~Zigbee();
+        void start(int serial_port, int softSerial_port);
+        
+        uint8_t sendCommand(String command);
+        uint8_t sendBinary(uint8_t data, int data_length);
 };
 
 #endif
