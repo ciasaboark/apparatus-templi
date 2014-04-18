876d5b30d4c73c017fab90dc8b34eca78282dca0
Christopher Hagler <haglerchristopher@gmail.com>
2014-04-18 18:49:58 -0400

    Update zigbee.h
    fixed a few errors

--- skeleton/arduino-sketches/echo/zigbee.h
+++ skeleton/arduino-sketches/echo/zigbee.h
@@ -1,32 +1,33 @@
 #ifndef ZIGBEE_H
 #define ZIGBEE_H
 
-#include <XBee.h>
-#include <SoftwareSerial.h>
-#include "Message.h"
-
+/* global defines */
 #define COORDINATOR_ADDRESS 0x00000000
 #define XBEE_PIN 12
 #define MAX_DATA_SIZE = 32
 
-class Zigbee {
-    private:
-        Xbee xbee;
-        SoftwareSerial softSerial;
-        XBeeResponse response;
-        ZBRxResponse rx;
-        ModemStatusResponse msr;
-        char name[10];
+#include <XBee.h>
+#include <SoftwareSerial.h>
+#include "Message.h"
 
+class Zigbee {
     public:
 	/* newName can only have a max character limit of 10. */
-        Zigbee(String newNname, int rx_pin, int tx_pin)
+        Zigbee(char *name, int rx_pin, int tx_pin);
 	~Zigbee();
 
-        void start(int serial_port);
-        void sendCommand(String command);
+        void start(int buad_rate = 9600); //start the serial communication with the default data transmission rate
+        void sendCommand(char *command);
         void sendBinary(uint8_t *data, int data_length);
-	void sendMessageFragment(Message *obj);
+        void sendMessageFragment(Message *obj);
+		
+	private:
+        Xbee 		    *xbee;
+        SoftwareSerial 	    *softSerial;
+        XBeeResponse 	    *response;
+        ZBRxResponse 	    *rx;
+        ModemStatusResponse *msr;
+        char name[10];
 };
 
 #endif
