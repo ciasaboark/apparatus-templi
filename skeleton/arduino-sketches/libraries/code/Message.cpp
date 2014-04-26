3c28d43ec7879ebc9e8619111dba1b18c4c344df
Christopher Hagler <haglerchristopher@gmail.com>
2014-04-25 22:02:37 -0400

    Update Message.cpp
    Fixed a few bugs with the setPayload and setDestinationName where they did not allocate their own space.

--- skeleton/arduino-sketches/libraries/code/Message.cpp
+++ skeleton/arduino-sketches/libraries/code/Message.cpp
@@ -5,11 +5,32 @@ Message::Message(uint8_t sigByte, uint8_t option, uint8_t length, uint16_t frag_
 	optionByte = option;
 	data_length = length;
 	fragment_number = frag_number;
-	destination_name = destination_address; //this probably will not work
-	payload = payld;
+	destination_name = (char*)malloc(11);
+	
+	if (destination_name != NULL) {
+		memset(destination_name, NULL, 11);
+		memcpy(destination_name, destination_address, 10);
+	}
+	
+
+	payload = (uint8_t*)malloc(length);
+	if (payload != NULL) {
+		memcpy(payload, payld, length);
+	}
+	/* remember to remove these statements */
+	Serial.println("the two print statements below are frm message.cpp constructor")
+	Serial.println("we are verifying the first byte of the payload");
+	Serial.print("The payld[0] before we malloc and assign it to message is: ");
+	Serial.println(payld[0]);
+	Serial.print("The payload[0] after we malloc and now message has it: ");
+	Serial.println(payload[0]);
+	Serial.println("if the two values above are the same then the data is consistent after copying the data over to the message object");
 }
 
-Message::~Message() { }
+Message::~Message() {
+	delete payload;
+	delete destination_name;
+}
 
 void Message::setSignatureByte(uint8_t sigByte) {
 	signature = sigByte;
@@ -27,12 +48,14 @@ uint8_t Message::getOptionByte() {
 	return optionByte;
 }
 
+
 void Message::setDestinationName(char *module) {
 	if(destination_name != NULL) {
 		delete (destination_name);
 	}
-	destination_name = (char*)malloc(sizeof(char) * 10);
+	destination_name = (char*)malloc(sizeof(char) * 11);
 	if(destination_name != NULL) {
+		memset(destination_name, NULL, 11);
 		memcpy(destination_name, module, 10);
 	}
 }
@@ -57,11 +80,15 @@ uint16_t Message::getFragmentNumber() {
 	return fragment_number;
 }
 
-void Message::setPayload(uint8_t *buffer) {
+void Message::setPayload(uint8_t *buffer, int length) {
 	if(payload != NULL) {
 		delete payload;
 	}
-	payload = buffer;
+	payload = (char*)malloc(sizeof(length))
+	if(payload != NULL) {
+		memset(payload, NULL, 11); //NULL out payload. This is not needed but I will remove after testing
+		memcpy(payload, buffer, 10);
+	}
 }
 
 uint8_t* Message::getPayload() {
