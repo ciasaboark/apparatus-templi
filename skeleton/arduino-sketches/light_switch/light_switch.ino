eb1700eb6c2cacea9e4427637b75676bb0bf1d7a
Christopher Hagler <haglerchristopher@gmail.com>
2014-04-29 18:04:50 -0400

    fixed the errors in the zigbee fragmentation in the receiveComamnd method. There is still a bug in the sendCommand when the message is sent as multiple fragments.

--- skeleton/arduino-sketches/light_switch/light_switch.ino
+++ skeleton/arduino-sketches/light_switch/light_switch.ino
@@ -6,8 +6,6 @@
 Servo servo;  
 
 int pos = 0;  //pos should either be 0 or 180, never any value in between
-int light_on_pin = 7;
-int light_off_pin = 6;
 
 Zigbee zigbee("light", 10, 11);
  
@@ -16,39 +14,50 @@ void setup() {
   Serial.begin(115200);
   zigbee.start(); //defaults to 9600
   
-  servo.attach(4); 
-  servo.write(pos);  
+  servo.attach(9); 
+  servo.write(0);  
   
-  pinMode(light_on_pin, OUTPUT);
-  digitalWrite(light_off_pin, HIGH);
-  pinMode(light_off_pin, OUTPUT); //set the start state to off
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT); //set the start state to off
+  digitalWrite(6, HIGH);
 } 
- 
  
 void loop() {  
   Message *message = zigbee.receiveMessage();
   uint8_t *data = message->getPayload();
   
-  if( (data != NULL) && (data[0] == 1) && (pos != 180) ) { //turn the light on
-    pos = 180;
-    servo.write(pos);
-    delay(2000);
-    pinMode(light_on_pin, HIGH);
-    pinMode(light_off_pin, LOW);
-    Serial.print("turning on the light");
+  if(message != NULL) {
+    if(data[0] == 49) { //turn the light on
+      pos = 180;
+      servo.write(pos);
+      delay(1000);
+      digitalWrite(7, HIGH);
+      digitalWrite(6, LOW);
+      
+      Serial.println("turning on the light");
+    }
+    else if(data[0] == 48) { //turn the light off
+      pos = 0;
+      servo.write(pos);
+      delay(1000);
+      digitalWrite(7, LOW);
+      digitalWrite(6, HIGH);
+      Serial.println("turning off the light");
+    }
+    else {
+      Serial.println("bad command");
+      for(int i = 0; i < 3; i++) {
+        digitalWrite(7, HIGH);
+        digitalWrite(6, HIGH);
+        delay(1000);
+        digitalWrite(7, LOW);
+        digitalWrite(6, LOW);
+        delay(1000);
+        String string = String("123456789012345678901234567890123456789012345678901234567890123456789");
+        zigbee.sendCommand(string);
+      }      
+    }
+  
+    delete message; //free the memory occupied by message. It is OK to delete a null pointer
   }
-  else if( (data != NULL) && (data[0] == 0) && (pos != 0) ) { //turn the light off
-    pos = 0;
-    servo.write(pos);
-    delay(2000);
-    pinMode(light_on_pin, LOW);
-    pinMode(light_off_pin, HIGH);
-    Serial.print("turning off the light");
-  }
-  else {
-    Serial.print("bad command");
-    //bad command
-  }
-  delete message; //free the memory occupied by message. It is OK to delete a null pointer
-  Serial.print("freeing the memory held by message");
-}
+} 
